2e706ad7580586be44216473a173a99d7b52d4ac
dingyi <dingyi222666@foxmail.com>
2024-07-19 03:57:14 +0800

    feat(service): extend Plugin API and improve client initialization
    - Extend the ChatLunaPlatformPlugin class to support additional configuration options.
    - Add utility methods for client creation and registration within the platform service.
    - Refactor the chat interface and conversation queue management for better performance.
    - Improve the handling of model types and embeddings during client initialization.
    - Remove deprecated code and clean up the service and chat interface implementations.

--- packages/service/src/service/chatluna.ts
+++ packages/service/src/service/chatluna.ts
@@ -2,12 +2,12 @@ import { Context, Service } from 'cordis'
 import { PlatformService } from '@chatluna/core/service'
 import { ChatLunaConversation } from '@chatluna/memory/types'
 import { ChatInterface, ChatInterfaceInput } from '@chatluna/chat/chat'
-import { ObjectLock, RequestQueue } from '@chatluna/utils'
+import { ChatLunaError, ChatLunaErrorCode, RequestQueue } from '@chatluna/utils'
 import crypto from 'crypto'
 import { AIMessage, HumanMessage } from '@langchain/core/messages'
 import { ChainEvents, ChatLunaLLMChainWrapper } from '@chatluna/core/chain'
 import { VectorStoreRetrieverMemory } from '@chatluna/core/memory'
-import { parseRawModelName, PromiseLikeDisposable } from '@chatluna/core/utils'
+import { parseRawModelName } from '@chatluna/core/utils'
 import {
     BasePlatformClient,
     ChatLunaTool,
@@ -18,17 +18,185 @@ import {
     CreateVectorStoreFunction,
     ModelType
 } from '@chatluna/core/platform'
-import { ChatLunaChatModel, ChatLunaEmbeddings } from '@chatluna/core/model'
+import {
+    ChatHubBaseEmbeddings,
+    ChatLunaChatModel,
+    ChatLunaEmbeddings
+} from '@chatluna/core/model'
+import {
+    ChatMiddlewareExecutor,
+    ChatMiddlewareGraph
+} from '@chatluna/chat/middleware'
+import path from 'path'
+import fs from 'fs'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export class ChatLunaService extends Service {
     private _platformPlugins: ChatLunaPlatformPlugin[] = []
     private _chatInterfaceWrapper: Record<string, ChatInterfaceWrapper> = {}
-    private _lock = new ObjectLock()
+    // private _lock = new ObjectLock()
     private readonly _chatMiddlewareExecutor: ChatMiddlewareExecutor
 
     constructor(ctx: Context) {
         super(ctx, 'chatluna')
+
+        this._chatMiddlewareExecutor = new ChatMiddlewareExecutor(
+            ctx,
+            new ChatMiddlewareGraph()
+        )
+
+        this._createTempDir()
+    }
+
+    async installPlatformPlugin(plugin: ChatLunaPlatformPlugin) {
+        this._platformPlugins.push(plugin)
+        this.ctx.logger.success(`register plugin %c`, plugin.platformName)
+    }
+
+    async removePlatformPlugin(plugin: ChatLunaPlatformPlugin) {
+        this._platformPlugins.splice(this._platformPlugins.indexOf(plugin), 1)
+
+        this.ctx.logger.success('unregister plugin %c', plugin.platformName)
+    }
+
+    findPlugin(
+        fun: (plugin: ChatLunaPlatformPlugin) => boolean
+    ): ChatLunaPlatformPlugin {
+        return this._platformPlugins.find(fun)
+    }
+
+    chat(
+        conversation: ChatLunaConversation,
+        message: HumanMessage,
+        event: ChainEvents,
+        stream: boolean = false,
+        signal?: AbortSignal,
+        chatMemory?: VectorStoreRetrieverMemory,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        params?: Record<string, any>
+    ) {
+        const { model: modelName } = conversation
+
+        // provider
+        const [platform] = parseRawModelName(modelName)
+
+        const chatInterfaceWrapper = this._chatInterfaceWrapper[platform]
+
+        if (chatInterfaceWrapper == null) {
+            throw new ChatLunaError(
+                ChatLunaErrorCode.MODEL_ADAPTER_NOT_FOUND,
+                new Error(`The platform ${platform} no available`)
+            )
+        }
+
+        return chatInterfaceWrapper.chat(
+            conversation,
+            message,
+            event,
+            stream,
+            signal,
+            chatMemory,
+            params
+        )
+    }
+
+    queryInterfaceWrapper(
+        conversation: ChatLunaConversation
+    ): ChatInterfaceWrapper | undefined {
+        const { model: modelName } = conversation
+
+        // provider
+        const [platform] = parseRawModelName(modelName)
+
+        return this._chatInterfaceWrapper[platform]
+    }
+
+    async clearChatHistory(conversation: ChatLunaConversation) {
+        const { model: modelName } = conversation
+
+        // provider
+        const [platformName] = parseRawModelName(modelName)
+
+        const chatBridger = this._chatInterfaceWrapper[platformName]
+
+        return chatBridger?.clearChatHistory(conversation)
+    }
+
+    getCachedInterfaceWrappers() {
+        return Object.values(this._chatInterfaceWrapper)
+    }
+
+    async clearCache(conversation: ChatLunaConversation) {
+        const { model: modelName } = conversation
+
+        // provider
+        const [platformName] = parseRawModelName(modelName)
+
+        const chatBridger = this._chatInterfaceWrapper[platformName]
+
+        return chatBridger?.clearCache(conversation)
+    }
+
+    async createModel(platformName: string, model: string) {
+        const service = this.ctx.chatluna_platform
+
+        const client = await service.randomClient(platformName)
+
+        if (client == null) {
+            throw new ChatLunaError(
+                ChatLunaErrorCode.MODEL_ADAPTER_NOT_FOUND,
+                new Error(`The platform ${platformName} no available`)
+            )
+        }
+
+        return client.createModel(model)
+    }
+
+    async createEmbeddings(platformName: string, modelName: string) {
+        const service = this.ctx.chatluna_platform
+
+        const client = await service.randomClient(platformName)
+
+        if (client == null) {
+            throw new ChatLunaError(
+                ChatLunaErrorCode.MODEL_ADAPTER_NOT_FOUND,
+                new Error(`The platform ${platformName} no available`)
+            )
+        }
+
+        const model = client.createModel(modelName)
+
+        if (model instanceof ChatHubBaseEmbeddings) {
+            return model
+        }
+
+        throw new ChatLunaError(
+            ChatLunaErrorCode.MODEL_NOT_FOUND,
+            new Error(`The model ${modelName} is not embeddings`)
+        )
+    }
+
+    createChatInterfaceWrapper(
+        platform: string,
+        input: ChatInterfaceInput
+    ): ChatInterfaceWrapper {
+        const chatBridger = new ChatInterfaceWrapper(this.ctx, input)
+        this.ctx.logger.debug(`create platform %c`, platform)
+        this._chatInterfaceWrapper[platform] = chatBridger
+        return chatBridger
+    }
+
+    private _createTempDir() {
+        // create dir data/chathub/temp use fs
+        // ?
+        const tempPath = path.resolve(this.ctx.baseDir, 'data/chatluna/temp')
+        if (!fs.existsSync(tempPath)) {
+            fs.mkdirSync(tempPath, { recursive: true })
+        }
+    }
+
+    middlewareExecutor<T, R>() {
+        return this._chatMiddlewareExecutor as ChatMiddlewareExecutor<T, R>
     }
 }
 
@@ -36,7 +204,7 @@ export class ChatLunaPlatformPlugin<
     R extends ClientConfig = ClientConfig,
     T extends ChatLunaPlatformPlugin.Config = ChatLunaPlatformPlugin.Config
 > {
-    private _disposables: PromiseLikeDisposable[] = []
+    private _disposables: (() => void)[] = []
 
     private _supportModels: string[] = []
 
@@ -51,7 +219,7 @@ export class ChatLunaPlatformPlugin<
         createConfigPool: boolean = true
     ) {
         ctx.once('dispose', async () => {
-            // await ctx.chatluna.unregisterPlugin(this)
+            ctx.chatluna.removePlatformPlugin(this)
         })
 
         // inject to root ctx
@@ -78,6 +246,10 @@ export class ChatLunaPlatformPlugin<
         }
     }
 
+    install() {
+        this.ctx.chatluna.installPlatformPlugin(this)
+    }
+
     async initClients() {
         this._platformService.registerConfigPool(
             this.platformName,
@@ -87,7 +259,7 @@ export class ChatLunaPlatformPlugin<
         try {
             await this._platformService.createClients(this.platformName)
         } catch (e) {
-            await this.onDispose()
+            this.uninstall()
             // await this.ctx.chatluna.unregisterPlugin(this)
 
             throw e
@@ -99,7 +271,7 @@ export class ChatLunaPlatformPlugin<
                 .map((model) => `${this.platformName}/${model.name}`)
         )
 
-        //  this.ctx.chatluna.registerPlugin(this)
+        this.install()
     }
 
     async initClientsWithPool<A extends ClientConfig = R>(
@@ -118,7 +290,7 @@ export class ChatLunaPlatformPlugin<
         try {
             await this._platformService.createClients(platformName)
         } catch (e) {
-            await this.onDispose()
+            await this.uninstall()
 
             throw e
         }
@@ -134,11 +306,12 @@ export class ChatLunaPlatformPlugin<
         return this._supportModels
     }
 
-    async onDispose(): Promise<void> {
+    uninstall() {
         while (this._disposables.length > 0) {
             const disposable = this._disposables.pop()
-            await disposable()
+            disposable()
         }
+        this.ctx.chatluna.removePlatformPlugin(this)
     }
 
     registerConfigPool(platformName: string, configPool: ClientConfigPool) {
@@ -160,17 +333,17 @@ export class ChatLunaPlatformPlugin<
         this._disposables.push(disposable)
     }
 
-    async registerVectorStore(name: string, func: CreateVectorStoreFunction) {
+    registerVectorStore(name: string, func: CreateVectorStoreFunction) {
         const disposable = this._platformService.registerVectorStore(name, func)
         this._disposables.push(disposable)
     }
 
-    async registerTool(name: string, tool: ChatLunaTool) {
+    registerTool(name: string, tool: ChatLunaTool) {
         const disposable = this._platformService.registerTool(name, tool)
         this._disposables.push(disposable)
     }
 
-    async registerChatChainProvider(
+    registerChatChainProvider(
         name: string,
         description: string,
         func: (
@@ -208,7 +381,6 @@ class ChatInterfaceWrapper {
 
     constructor(
         private _ctx: Context,
-        private _service: ChatLunaService,
         private _input: ChatInterfaceInput
     ) {
         this._platformService = this._ctx.chatluna_platform
